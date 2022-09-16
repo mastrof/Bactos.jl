3cdc34bc6c7ce7c1b1a02003b3739e07eabbfcd8
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-09-16 11:58:40 +0200

    update motility structs

--- src/motility.jl
+++ src/motility.jl
@@ -2,23 +2,53 @@ export
     AbstractMotility, AbstractMotilityOneStep, AbstractMotilityTwoStep,
     RunTumble, RunReverse, RunReverseFlick
 
+"""
+    AbstractMotility
+General abstract interface for motility patterns.
+"""
 abstract type AbstractMotility end
+
+"""
+    AbstractMotilityOneStep
+One-step motility patterns (such as run-tumble and run-reverse).
+Subtypes must have at least the following fields:
+- `speed`: distribution of microbe speed, new values extracted after each turn
+- `yaw`: distribution of in-plane reorientations
+- `pitch`: distribution of out-of-plane reorientations
+For 2-dimensional microbe types, only `yaw` defines reorientations.
+"""
 abstract type AbstractMotilityOneStep end
 abstract type AbstractMotilityTwoStep end
 
 Base.@kwdef struct RunTumble <: AbstractMotilityOneStep
     speed = Degenerate(1.0)
-    angle = Uniform(-π, π)
+    yaw = Uniform(-π, π) # in-plane
+    pitch = Uniform(0,2π) # out-of-plane
 end # struct
 
 Base.@kwdef struct RunReverse <: AbstractMotilityOneStep
     speed = Degenerate(1.0)
-    angle = Degenerate(π)
+    yaw = Degenerate(π)
+    pitch = Uniform(0,2π)
 end # strut 
 
+"""
+    AbstractMotilityTwoStep
+Two-step motility patterns (such as run-reverse-flick).
+Subtypes must have at least the following fields:
+- `speed`: distribution of microbe speed, new values extracted after each turn
+- `yaw_0`: distribution of in-plane reorientations for motile state 0
+- `pitch_0`: distribution of out-of-plane reorientations for motile state 0
+- `yaw_1`: distribution of in-plane reorientations for motile state 1
+- `pitch_1`: distribution of out-of-plane reorientations for motile state 1
+- `motile_state::Vector{Int}`: defines current motile state (`[0]` or `[1]`)
+For 2-dimensional microbe types, only `yaw_0` and `yaw_1` define reorientations.
+"""
 Base.@kwdef struct RunReverseFlick <: AbstractMotilityTwoStep
     speed = Degenerate(1.0)
-    angle_reverse = Degenerate(π)
-    angle_flick = Degenerate(π/2)
+    yaw_0 = Degenerate(π)
+    pitch_0 = Uniform(0,2π)
+    yaw_1 = Degenerate(π/2)
+    pitch_1 = Uniform(0,2π)
     motile_state::Vector{Int} = rand(0:1, 1)
 end # struct 
