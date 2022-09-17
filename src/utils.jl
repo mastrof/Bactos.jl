b8b665b42c69fbbfae84620c0111c86cc9b200bf
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-09-17 16:37:56 +0200

    add docstring

--- src/utils.jl
+++ src/utils.jl
@@ -1,6 +1,11 @@
 export
     rand_vel
 
+"""
+    rand_vel(N)
+    rand_vel(rng, N)
+Generate a random N-tuple of unitary norm.
+"""
 function rand_vel(D)
     v = rand(Random.default_rng(), D) .* 2 .- 1
     Tuple(v ./ norm(v))
