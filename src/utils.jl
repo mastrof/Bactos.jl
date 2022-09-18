3d4b75294aa74b8e137c18caf076156373c7f364
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-09-18 10:38:26 +0200

    add vectorize_adf_measurement

--- src/utils.jl
+++ src/utils.jl
@@ -1,5 +1,5 @@
 export
-    rand_vel
+    rand_vel, vectorize_adf_measurement
 
 """
     rand_vel(N)
@@ -14,4 +14,22 @@ end # function
 function rand_vel(rng, D)
     v = rand(rng, D) .* 2 .- 1
     Tuple(v ./ norm(v))
+end # function
+
+"""
+    vectorize_adf_measurement(adf, sym)
+Collect quantity `sym` from the agent dataframe `adf` and return it in matrix 
+form with dimensions (microbes, times).
+"""
+function vectorize_adf_measurement(adf, sym)
+    nmicrobes = unique(adf[!,:id]) |> length
+    nsteps = unique(adf[!,:step]) |> length
+    datatype = typeof(adf[1,sym])
+    s = Matrix{datatype}(undef, nmicrobes, nsteps)
+    for t in 1:nsteps
+        for i in 1:nmicrobes
+            s[i,t] = adf[i + (t-1)*nmicrobes, sym]
+        end # for
+    end # for
+    return s
 end # function
