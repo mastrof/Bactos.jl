0a1a1836abf539fd28578c29b5fb639d12dc2f71
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-09-18 16:06:32 +0200

    add example random walk 2d

---
+++ examples/random_walk_2d.jl
@@ -0,0 +1,34 @@
+using BacteriaBasedModels
+using Agents: run!
+using Plots
+
+L = 50.0
+τ_run = 1.0
+ω = 1 / τ_run
+nmicrobes = 6
+microbes = [
+    Microbe{2}(
+        id=i, vel=rand_vel(2), turn_rate=ω,
+        motility=RunTumble()
+    ) for i in 1:nmicrobes
+]
+
+model = initialise_model(;
+    microbes = microbes,
+    timestep = 1.0,
+    extent = L, periodic = false,
+    random_positions = true,
+)
+
+nsteps = 200
+adata = [:pos]
+adf, = run!(model, microbe_step!, nsteps; adata)
+
+trajectories = vectorize_adf_measurement(adf, :pos)
+x = first.(trajectories)
+y = last.(trajectories)
+
+plot(x', y',
+    lw=0.5,
+    lab=false, ticks=false, lims=(0,L)
+)
