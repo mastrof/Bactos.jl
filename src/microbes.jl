fd0a5ef8b519b680210a413d0aaa27324f651dc5
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-09-19 10:17:44 +0200

    update docstring

--- src/microbes.jl
+++ src/microbes.jl
@@ -1,19 +1,20 @@
 export AbstractMicrobe, Microbe
 
 """
-    YourMicrobeType{D} <: AbstractMicrobe{D} where {D<:Integer}
+    AbstractMicrobe{D} <: AbstractAgent where {D<:Integer}
+    YourMicrobeType{D} <: AbstractMicrobe{D}
 All microbe types in BacteriaBasedModels.jl simulations must be instances
 of user-defined types that are subtypes of `AbstractMicrobe`.
 The parameter `D` defines the dimensionality of the space in which the
 microbe type lives (1, 2 and 3 are currently supported).
 
 All microbe types should have the following fields:
-`id::Int` → id of the microbe
-`pos::NTuple{D,Float64}` → position of the microbe
-`vel::NTuple{D,Float64}` → velocity of the microbe
-`motility` → motile pattern of the microbe
-`turn_rate::Float64` → average reorientation rate of the microbe
-`rotational_diffusivity::Float64` → rotational diffusion coefficient
+`- id::Int` → id of the microbe
+`- pos::NTuple{D,Float64}` → position of the microbe
+`- vel::NTuple{D,Float64}` → velocity of the microbe
+`- motility` → motile pattern of the microbe
+`- turn_rate::Float64` → average reorientation rate of the microbe
+`- rotational_diffusivity::Float64` → rotational diffusion coefficient
 """
 abstract type AbstractMicrobe{D} <: AbstractAgent where {D<:Integer} end
 
@@ -21,6 +22,7 @@ abstract type AbstractMicrobe{D} <: AbstractAgent where {D<:Integer} end
 """
     Microbe{D} <: AbstractMicrobe{D}
 Basic microbe type for simple simulations.
+
 Default parameters:
 - `id::Int` → identifier used internally by Agents.jl
 - `pos::NTuple{D,Float64} = ntuple(zero,D)` → position
