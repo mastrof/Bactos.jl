6d3cd01b25fa14e3346385b5cfa971ed3d8cb9e2
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-09-25 17:53:23 +0200

    add msd tests

--- test/measurements.jl
+++ test/measurements.jl
@@ -37,4 +37,41 @@ using Test, BacteriaBasedModels, Random
         @test vd3_pnt_n[1] == 1.0
         @test vd3_pnt_n[3] == -1.0
     end
+
+    @testset "Mean-squared displacement" begin
+        U = 2.0
+        extent = 50.0
+        microbes = [Microbe{1}(id=0, turn_rate=0, vel=(U,), pos=(extent/2,))]
+        timestep = 0.1
+        model = initialise_model(; microbes, extent, timestep,
+                                   random_positions=false)
+        adata = [:pos]
+        nsteps = 10
+        adf, = run!(model, microbe_step!, nsteps; adata)
+        Δx = adf[end,:pos][1] .- adf[1,:pos][1]
+        @test Δx ≈ U*nsteps*timestep
+        MSD = msd(adf) |> vec
+        for n in 1:nsteps
+            @test MSD[n] ≈ (U*n*timestep)^2
+        end # for
+
+        nsteps = 500
+        adf, = run!(model, microbe_step!, nsteps; adata)
+        MSD = msd(adf; L=extent) |> vec
+        @test MSD[nsteps] ≈ (U*nsteps*timestep)^2
+
+        U = 3.0
+        microbes = [Microbe{1}(id=0, turn_rate=Inf, vel=(U,),
+                    motility=RunTumble(speed=Degenerate(U)))]
+        extent = 50.0
+        timestep = 0.1 
+        model = initialise_model(; microbes, extent, timestep)
+        nsteps = 4
+        adf, = run!(model, microbe_step!, nsteps; adata)
+        MSD = msd(adf; L=extent) |> vec 
+        @test MSD[1] ≈ (U*timestep)^2
+        @test MSD[2] ≈ 0.0
+        @test MSD[3] ≈ MSD[1]
+        @test MSD[4] ≈ MSD[2]
+    end
 end
