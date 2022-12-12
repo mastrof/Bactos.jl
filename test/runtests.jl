c079dc3b8faaf1733ace0d1b3829d84fde086edc
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-12-12 11:31:29 +0100

    add tests for motile types

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Test, BacteriaBasedModels, Random
 
 @testset "BacteriaBasedModels Tests" begin
     include("microbe_creation.jl")
+    include("motility.jl")
     include("model_creation.jl")
     include("reorientations.jl")
     include("step.jl")
