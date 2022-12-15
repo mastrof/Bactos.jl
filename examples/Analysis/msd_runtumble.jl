9268b94aa6c18c54e4b1013f4e31af73611fe9b7
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-12-15 11:46:47 +0100

    Fix analysis examples (#43)
    * update msd example
    
    * update autocorrelation example

--- examples/Analysis/msd_runtumble.jl
+++ examples/Analysis/msd_runtumble.jl
@@ -3,19 +3,18 @@ using Distributions
 using Plots
 
 θs = [π/6, π/4, π/3, π/2, π]
+α = cos.(θs)
 
 U = 30.0 # μm/s 
-τ_run = 1.0 # s 
-ω = 1 / τ_run
+τ = 1.0 # s 
+ω = 1 / τ
 
 nmicrobes = 100
 microbes = [
     [
         Microbe{3}(
             id = n, turn_rate = ω,
-            motility = RunTumble(
-                speed=Degenerate(U), polar=[θ,-θ],
-                )
+            motility = RunTumble(speed=[U], polar=[θ,-θ])
         ) for n in 1:nmicrobes 
     ] for θ in θs
 ]
@@ -33,23 +32,27 @@ nsteps = round(Int, 100τ_run / dt)
 adata = [:pos]
 adfs = [run!(model, microbe_step!, nsteps; adata)[1] for model in models]
 
-MSD = msd.(adfs; L=L)
-ts = (1:nsteps).*dt
+MSD = hcat(msd.(adfs; L=L)...)
 
-logslice = [1,2,5,10,25,50,100,250,500,1000]
-plot(
-    xlab = "Δt (s)",
-    ylab = "MSD (μm²/s)",
-    legend = :bottomright, legendtitle = "1-cosθ",
-    scale = :log10
-)
-scatter!(ts[logslice], hcat(MSD...)[logslice,:],
-    m=:x, ms=6, msw=2, lab=false)
-for i in eachindex(θs)
-    α = 1 - cos(θs[i])
-    τ = τ_run / α
-    D = U^2*τ / 3
-    dr² = @. 2*U^2*τ^2 * (ts/τ - 1 + exp(-ts/τ))
-    plot!(ts, dr², lab=round(α,digits=2), lc=i, lw=2)
-end # for
-plot!()
+begin  
+    t = (1:nsteps).*dt
+    T = @. τ / (1-α')
+    s = t ./ T
+    D = @. U^2*T/3
+    MSD_theoretical = @. 6D*T * (s - 1 + exp(-s))
+    logslice = round.(Int, exp10.(range(0,3,length=10)))
+    plot(
+        xlab = "Δt / τ",
+        ylab = "MSD / (Uτ)²",
+        legend = :bottomright, legendtitle = "1-α",
+        scale = :log10,
+        yticks = exp10.(-2:2:2),
+        xticks = exp10.(-2:2)
+    )
+    scatter!(t[logslice,:]./τ, MSD[logslice,:]./(U*τ)^2,
+        m=:x, ms=6, msw=2, lab=false, lc=axes(α,1)'
+    )
+    plot!(t./τ, MSD_theoretical./(U*τ)^2,
+        lw=2, lab=round.(1 .- α,digits=2)', lc=axes(α,1)'
+    )
+end
