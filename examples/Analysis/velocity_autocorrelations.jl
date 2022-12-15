77557717b11b76fd7f261968bff8ed2ce95365eb
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-12-15 11:43:07 +0100

    update autocorrelation example

--- examples/Analysis/velocity_autocorrelations.jl
+++ examples/Analysis/velocity_autocorrelations.jl
@@ -5,27 +5,24 @@ U = 30.0 # μm/s
 τ_run = 1.0 # s
 ω = 1 / τ_run # 1/s
 Δt = 0.01 # s
-L = 1e4 # μm
+extent = 1e4 # μm
 
 n = 200
 microbes_runtumble = [
     Microbe{3}(id=i,
-        turn_rate=ω, vel=rand_vel(3).*U,
-        motility=RunTumble(speed=Degenerate(U))
+        turn_rate=ω, motility=RunTumble(speed=[U])
     )
     for i in 1:n
 ]
 microbes_runrev = [
     Microbe{3}(id=n+i,
-        turn_rate=ω, vel=rand_vel(3).*U,
-        motility=RunReverse(speed=Degenerate(U))
+        turn_rate=ω, motility=RunReverse(speed_forward=[U])
     )
     for i in 1:n
 ]
 microbes_runrevflick = [
     Microbe{3}(id=2n+i,
-        turn_rate=ω, vel=rand_vel(3).*U,
-        motility=RunReverseFlick(speed=Degenerate(U))
+        turn_rate=ω, motility=RunReverseFlick(speed_forward=[U])
     )
     for i in 1:n
 ]
@@ -35,25 +32,25 @@ microbes = vcat(
 )
 
 model = initialise_model(;
-    microbes = microbes,
-    timestep = Δt,
-    extent = L, periodic = true
+    microbes,
+    extent, periodic = true,
+    timestep = Δt
+
 )
 
 nsteps = round(Int, 100τ_run / Δt)
-adata = [:pos, :vel]
+adata = [:vel]
 adf, = run!(model, microbe_step!, nsteps; adata)
 
-adf_runtumble = filter(:id => id -> 1≤id≤n, adf; view=true)
-adf_runrev = filter(:id => id -> n+1≤id≤2n, adf; view=true)
-adf_runrevflick = filter(:id => id -> 2n+1≤id≤3n, adf; view=true)
-
+adf_runtumble = filter(:id => id -> model.agents[id].motility isa RunTumble, adf; view=true)
+adf_runrev = filter(:id => id -> model.agents[id].motility isa RunReverse, adf; view=true)
+adf_runrevflick = filter(:id => id -> model.agents[id].motility isa RunReverseFlick, adf; view=true)
 adfs = [adf_runtumble, adf_runrev, adf_runrevflick]
 
 Φ = hcat([autocorrelation(a,:vel) for a in adfs]...)
 
 t = range(0, (nsteps-1)*Δt; step=Δt)
-ϕ = hcat([
+Φ_theoretical = hcat([
     exp.(-t ./ τ_run),
     exp.(-t ./ (τ_run / 2)),
     (1 .- t ./ (2τ_run)) .* exp.(-t ./ τ_run),
@@ -64,7 +61,6 @@ plot(
     xlab="Δt / τ",
     ylab="velocity autocorrelation",
 )
-plot!(t, ϕ, lw=2, lc=[1 2 3],
-    label=["Run-Tumble" "Run-Reverse" "Run-Reverse-Flick"])
-scatter!(t[1:10:end], Φ[1:10:end,:] ./ U^2, m=:x, mc=[1 2 3],
-    label=false)
+plot!(t, Φ_theoretical, lw=2, lc=[1 2 3], label=["Run-Tumble" "Run-Reverse" "Run-Reverse-Flick"])
+scatter!(t[1:15:end], Φ[1:15:end,:] ./ U^2, m=:x, mc=[1 2 3], label=false)
+hline!([0.0], lw=0.8, ls=:dash, lc=:black, lab=false)
