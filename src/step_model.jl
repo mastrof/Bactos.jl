7c7246e324320d25f3476ef286e50a4f4a788774
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-12-29 12:26:44 +0100

    increase `model.t` at each step

--- src/step_model.jl
+++ src/step_model.jl
@@ -8,11 +8,14 @@ If `model` contains an OrdinaryDiffEq integrator among its
 properties (`model.properties.integrator`), also perform an integration step.
 """
 function model_step!(model;
-    update_model!::Function = (model) -> nothing)
+    update_model!::Function = (model) -> nothing
+)
     # if a diffeq integrator is provided, integrate over a timestep
     if haskey(model.properties, :integrator)
         step!(model.integrator, model.timestep, true)
     end # if
     # update model properties
     update_model!(model)
+    # increase step count
+    model.t += 1
 end # function
