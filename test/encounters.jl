db5bc34ef2b9dfb3ae34402edefef6e4ef4c4b0b
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-12-30 02:13:53 +0100

    add tests for new `encounters!` and `reinsert!` functions

--- test/encounters.jl
+++ test/encounters.jl
@@ -65,5 +65,37 @@ using Test, Bactos
         model = initialise_model(; microbes, extent, timestep, random_positions)
         @test ~is_encounter(microbes[1], s₁, model)
         @test ~is_encounter(microbes[1], s₁, model)
+
+        # test the ABM interface
+        extent = L = 100
+        timestep = 0.1
+        random_positions = false
+        U = 30
+        r = 20
+        s = ObstacleSphere((L/2,L/2), r)
+        spheres = [s]
+        microbes = [Microbe{2}(id=1, pos=(L/2-r-U*timestep/2,L/2), vel=(U,0))]
+        x₀ = microbes[1].pos
+        model_properties = Dict(:bodies => spheres)
+        model = initialise_model(;
+            microbes, timestep, extent, random_positions,
+            model_properties
+        )
+        my_model_step!(model) = model_step!(model, update_model! = encounters!)
+        # an encounter should occur at first step
+        run!(model, microbe_step!, my_model_step!, 1)
+        @test model.encounters == 1
+        # and the microbe should be in a new position
+        @test ~all(model[1].pos .≈ x₀ .+ (U,0).*timestep)
+        # the new position is also not triggering an encounter in the next step
+        run!(model, microbe_step!, my_model_step!, 1)
+        @test model.encounters == 1
+        # test reinsert! explicitly
+        x₁ = model[1].pos
+        reinsert!(model[1], model, :bodies)
+        # new position which does not trigger an encounter
+        @test ~all(model[1].pos .≈ x₁)
+        run!(model, microbe_step!, my_model_step!, 1)
+        @test model.encounters == 1
     end
 end
