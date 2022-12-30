9ecbae6e87f5e6ac441c1a8b7cb46c77a7330f3a
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-12-30 01:41:08 +0100

    fix typos in 3d tests

--- test/encounters.jl
+++ test/encounters.jl
@@ -44,24 +44,24 @@ using Test, Bactos
         s₁ = ObstacleSphere(x₀, r₁)
         s₂ = ObstacleSphere(x₀, r₂)
         # microbe inside the sphere always triggers an encounter
-        microbes = [Microbe{2}(id=1, pos=(L/2,L/2,L/2))] # radius=0
+        microbes = [Microbe{3}(id=1, pos=(L/2,L/2,L/2))] # radius=0
         model = initialise_model(; microbes, extent, timestep, random_positions)
         @test is_encounter(microbes[1], s₁, model)
         @test is_encounter(microbes[1], s₂, model)
         # if microbe starts from surface contact, it always triggers an encounter
-        microbes = [Microbe{2}(id=1, pos=(L/2-r₁,L/2,L/2))]
+        microbes = [Microbe{3}(id=1, pos=(L/2-r₁,L/2,L/2))]
         model = initialise_model(; microbes, extent, timestep, random_positions)
         @test is_encounter(microbes[1], s₁, model)
         # if a microbe reaches contact distance, it triggers an encounter
-        microbes = [Microbe{2}(id=1, pos=(L/2-r₁-U*timestep,L/2,L/2), vel=(U,0,0))]
+        microbes = [Microbe{3}(id=1, pos=(L/2-r₁-U*timestep,L/2,L/2), vel=(U,0,0))]
         model = initialise_model(; microbes, extent, timestep, random_positions)
         @test is_encounter(microbes[1], s₁, model)
         # encounter triggered even if *both* endpoints are outside sphere
-        microbes = [Microbe{2}(id=1, pos=(L/2-r₂-0.5,L/2,L/2), vel=(U,0,0))]
+        microbes = [Microbe{3}(id=1, pos=(L/2-r₂-0.5,L/2,L/2), vel=(U,0,0))]
         model = initialise_model(; microbes, extent, timestep, random_positions)
         @test is_encounter(microbes[1], s₂, model)
         # if displacement does not cross the sphere, no encounter is triggered
-        microbes = [Microbe{2}(id=1, pos=(L/2-2r₁,L/2,L/2), vel=(0,U,0))]
+        microbes = [Microbe{3}(id=1, pos=(L/2-2r₁,L/2,L/2), vel=(0,U,0))]
         model = initialise_model(; microbes, extent, timestep, random_positions)
         @test ~is_encounter(microbes[1], s₁, model)
         @test ~is_encounter(microbes[1], s₁, model)
