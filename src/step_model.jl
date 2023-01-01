8e7c4a7c10725929990a2c371d982cb5d5e6c697
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-01-01 23:56:46 +0100

    reduce `model_step!` to only timekeeping and add distinct `diffeq_step!` function

--- src/step_model.jl
+++ src/step_model.jl
@@ -1,22 +1,30 @@
-export model_step!
+export model_step!, diffeq_step!
 
 """
-    model_step!(model; update_model! = (model) -> nothing)
-Update model properties through the `update_model!` function
-(defaults to nothing).
-If `model` contains an OrdinaryDiffEq integrator among its
-properties (`model.integrator`), also perform an integration step.
+    model_step!(model::ABM)
+Model stepping function, stored in `model.update!`.
+By default, it only increases the time count of `model` (`model.t += 1`).
+
+Extra functionalities can be added to the basic `model_step!` through
+function chaining e.g. `chain!(model, (f₁!, f₂!, f₃!))`.
+
+Some add-ons of general utility are already provided by Bactos.jl
+(see respective documentations for more details):
+* `diff_step!`: used to integrate differential equations if `model` has an
+    `integrator` property (`model.integrator`)
+* `update_neighborlist!`: update the microbe positions in a neighborlist
+* `surface_interaction!`: used to evaluate the effect of interactions between
+    microbes and other surfaces
 """
-function model_step!(model;
-    update_model!::Function = (model) -> nothing
-)
-    # if a diffeq integrator is provided, integrate over a timestep
-    if haskey(model.properties, :integrator)
-        step!(model.integrator, model.timestep, true)
-    end # if
-    # increase step count
+function model_step!(model::ABM)
     model.t += 1
-    # update model properties
-    update_model!(model)
     return nothing
 end # function
+
+"""
+    diffeq_step!(model::ABM)
+Integrate a differential equation over a timestep (`model.timestep`).
+The differential equation has to be defined through the integrator interface
+of DifferentialEquations.jl and stored in `model.integrator`.
+"""
+diffeq_step!(model::ABM) = step!(model.integrator, model.timestep, true)
